ddca83fe3be6d978fce3852a07673f191ce111fc
yangleyi <13243166527@sina.cn>
2018-09-29 18:19:18 +0800

    pay success step 1.

--- server/index.js
+++ server/index.js
@@ -7,8 +7,14 @@ app.use(bodyParser.json())
 
 app.post('/payment', (req, res) => {
   console.log('request api: ', req.body)
-  tool.unifiedorder()
-  res.send(`POST response... ${tool.nonce_str()}`)
+  let openid = req.body.openid 
+  if (openid) {
+    tool.unifiedorder(openid).then(data => {
+      console.log('success will send data to client',data)
+      res.send({result: data})
+    })
+  }
+  // res.send(`POST response... ${tool.nonce_str()}`)
 })
 
 let port = 9290
